0f91a7576165636ec0ff2b03dd903f985da88b37
rlwjd31 <rlwjd31@naver.com>
2025-06-09 05:44:07 +0900

    🚀 [Feat]: blog layout 기본 구조 설계 및 마크업 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,7 @@
 import { getPublishedPosts } from "@/lib/notion";
-import { Post } from "@/types/blog.type";
-import {
-  PageObjectResponse,
-  PersonUserObjectResponse,
-} from "@notionhq/client/build/src/api-endpoints";
 
 export default async function BlogListPage() {
   const response = await getPublishedPosts({});
-  console.log(response);
+  
   return <div>blog 목록 페이지</div>;
 }
