73893507aa4da8191278b51d2c02ddb76ba204fc
rlwjd31 <rlwjd31@naver.com>
2025-06-15 23:45:23 +0900

    ♻️ [Refactor]: blog mock data 추출
    추후 추가될 mock data의 관리를 도메인별로 src/mocks 디렉토리로에서 중앙 집중적으로 관리를 위해 추출

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,20 +1,10 @@
 import { Card } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
+import { blogs } from "@/mocks/blog";
+import { Blog } from "@/types/blog.type";
 import Link from "next/link";
 
-// Blog 타입 정의
-export type Blog = {
-  id: string;
-  title: string;
-  description?: string;
-  coverImage?: string;
-  tags?: string[];
-  author?: string;
-  date?: string;
-  modifiedDate?: string;
-  slug: string;
-};
 
 // BlogCard 컴포넌트
 function BlogCard({ blog }: { blog: Blog }) {
@@ -51,109 +41,7 @@ function BlogCard({ blog }: { blog: Blog }) {
   );
 }
 
-// Mock 데이터
-const blogs: Blog[] = [
-  {
-    id: "1",
-    title: "The Impact of Technology on the Workplace: How Technology is Changing",
-    description:
-      "An overview of blockchain technology and its potential impact on various industries.",
-    coverImage:
-      "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80",
-    tags: ["Technology"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "impact-of-technology",
-  },
-  {
-    id: "2",
-    title: "Exploring Venice: A Journey Through Canals",
-    description: "Discover the beauty and history of Venice through its iconic waterways.",
-    coverImage:
-      "https://images.unsplash.com/photo-1464983953574-0892a716854b?auto=format&fit=crop&w=600&q=80",
-    tags: ["Travel"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "exploring-venice",
-  },
-  {
-    id: "3",
-    title: "Urban Style: The Rise of Modern Fashion",
-    description: "A look into how urban culture is shaping the latest fashion trends.",
-    coverImage:
-      "https://images.unsplash.com/photo-1519125323398-675f0ddb6308?auto=format&fit=crop&w=600&q=80",
-    tags: ["Business"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "urban-style-fashion",
-  },
-  {
-    id: "4",
-    title: "Spring Cottage Retreats: Nature's Embrace",
-    description: "Experience tranquility in the countryside with these beautiful cottages.",
-    coverImage:
-      "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?auto=format&fit=crop&w=600&q=80",
-    tags: ["Travel"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "spring-cottage-retreats",
-  },
-  {
-    id: "5",
-    title: "Finding Balance: The Art of Rock Stacking",
-    description: "Discover the meditative practice and artistry behind rock stacking.",
-    coverImage:
-      "https://images.unsplash.com/photo-1465101046530-73398c7f28ca?auto=format&fit=crop&w=600&q=80",
-    tags: ["Sports"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "art-of-rock-stacking",
-  },
-  {
-    id: "6",
-    title: "Classic Cars: Timeless Beauty on Wheels",
-    description: "A celebration of classic cars and their enduring appeal.",
-    coverImage:
-      "https://images.unsplash.com/photo-1503736334956-4c8f8e92946d?auto=format&fit=crop&w=600&q=80",
-    tags: ["Economy"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "classic-cars-timeless-beauty",
-  },
-  {
-    id: "7",
-    title: "Tech Gadgets: The Future is Now",
-    description: "A roundup of the latest must-have tech gadgets for 2025.",
-    coverImage:
-      "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&w=600&q=80",
-    tags: ["Technology"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "tech-gadgets-future",
-  },
-  {
-    id: "8",
-    title: "Kayaking Adventures: Paddle into Paradise",
-    description: "Explore the world's most beautiful kayaking destinations.",
-    coverImage:
-      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=600&q=80",
-    tags: ["Travel"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "kayaking-adventures",
-  },
-  {
-    id: "9",
-    title: "Gaming Evolution: The Rise of Next-Gen Consoles",
-    description: "How next-gen consoles are changing the gaming landscape.",
-    coverImage:
-      "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?auto=format&fit=crop&w=600&q=80",
-    tags: ["Technology"],
-    author: "Tracey Wilson",
-    date: "1 June 2025",
-    slug: "gaming-evolution-nextgen",
-  },
-];
+
 
 export default function BlogListLayout({ children }: { children: React.ReactNode }) {
   // tag mock up data
